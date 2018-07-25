f3dc5ac12b4d14a606249e3adf6b08b3e003734c
Akigaze <1175520196@qq.com>
2018-07-25 21:50:27 +0800

    [Akigaze] add Car model

--- src/main/java/com/spring/parking/model/Car.java
+++ src/main/java/com/spring/parking/model/Car.java
@@ -1,4 +1,26 @@
 package com.spring.parking.model;
 
 public class Car {
+    private int id;
+    public Car(){
+
+    }
+    public Car(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Car){
+            final Car car=(Car)obj;
+            if (this.id==car.id){
+                return true;
+            }
+        }
+        return false;
+    }
 }
