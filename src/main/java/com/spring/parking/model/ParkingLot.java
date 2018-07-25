71872436dc1a6b5299bc6b3a619d1abecfb5d358
Akigaze <1175520196@qq.com>
2018-07-25 21:57:06 +0800

    [Akigaze] copy function to ParkingLot.java from previous project

--- src/main/java/com/spring/parking/model/ParkingLot.java
+++ src/main/java/com/spring/parking/model/ParkingLot.java
@@ -1,70 +1,93 @@
 package com.spring.parking.model;
 
+import com.spring.parking.model.exception.ParkingLotFullException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Service
 public class ParkingLot {
     private int id;
     private String name;
     private int capacity;
-    private List<Car> cars=new ArrayList<>();
+    private Map<Receipt,Car> parkedCars=new HashMap<>();
 
-    public ParkingLot() {
+    public ParkingLot(){}
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public ParkingLot(String name, int capacity) {
+        this((int)(Math.random()*100),name,capacity);
     }
 
     public ParkingLot(int id, String name, int capacity) {
-        this.id = id;
-        this.name = name;
-        this.capacity = capacity;
+        this.id=id;
+        this.name=name;
+        this.capacity=capacity;
     }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getFormatId() {
+        return String.format("%03d",id);
     }
+
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getCapacity() {
         return capacity;
     }
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
+    public int getCarNum() {
+        return parkedCars.size();
     }
 
-    public List<Car> getCars() {
-        return cars;
+    public Receipt park(Car car) {
+        if (parkedCars.size()<capacity){
+            Receipt card=new Receipt();
+            parkedCars.put(card,car);
+            return card;
+        }else {
+            throw new ParkingLotFullException();
+        }
     }
 
-    public void setCars(List<Car> cars) {
-        this.cars = cars;
+    public Car unpark(Receipt card) {
+        return parkedCars.remove(card);
     }
+
+    public boolean isFull() {
+        return parkedCars.size()==capacity;
+    }
+
+    public boolean containsReceipt(Receipt card){
+        return parkedCars.containsKey(card);
+    }
+
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        ParkingLot that = (ParkingLot) o;
-        return id == that.id;
+        ParkingLot lot = (ParkingLot) o;
+        return id == lot.id;
     }
 
     @Override
     public int hashCode() {
-
         return Objects.hash(id);
+    }
+
+
+    public boolean hasCar() {
+        return getCarNum()>0;
     }
 }
