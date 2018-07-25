24c35f094a43775656ffc761d7ca3cc2f3c47124
Akigaze <1175520196@qq.com>
2018-07-25 21:52:17 +0800

    [Akigaze] add Receipt model

---
+++ src/main/java/com/spring/parking/model/Receipt.java
@@ -0,0 +1,37 @@
+package com.spring.parking.model;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Receipt {
+
+        private String id;
+
+        public Receipt() {
+            this.id = UUID.randomUUID().toString();
+        }
+        public Receipt(String id) {
+            this.id = id;
+        }
+        public String getId() {
+            return id;
+        }
+
+        public void setID(String s) {
+            this.id=s;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Receipt card = (Receipt) o;
+            return Objects.equals(id, card.id);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
+
+}
